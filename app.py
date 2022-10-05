a5dbaaf841de56598943e9bcd7d91b113a753f0b
NeilAnthon <neilanthony.prohibido@lspu.edu.ph>
2022-10-05 18:26:38 +0800

    dsaasdds

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('./index.html')
 
 
+# Launch the FlaskPy dev server
 app.run(host="localhost", debug=True)
--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+import os
+
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, './')
+
+app = Flask(__name__, template_folder=template_path)
 
 
 @app.route('/')
@@ -8,4 +13,4 @@ def hello():
     return render_template('./index.html')
 
 
-app.run(host="localhost", debug=True)
+app.run(host='0.0.0.0', port=5000)
